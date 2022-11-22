9fc443b2a02d089102d3371bae41a012f1507eba
igli-corbaxhiu <igli.corbaxhiu@fti.edu.al>
2022-11-22 10:00:30 +0100

    commit

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+use Illuminate\Support\Facades\Gate;
+
+// use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        Gate::define('admin', function($user, $class, $roles) {
+            if( isset( $user->superuser ) && $user->superuser ) {
+                return true;
+            }
+            return app( '\Aimeos\Shop\Base\Support' )->checkUserGroup( $user, $roles );
+        });
+    }
+}
